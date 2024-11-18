97de5db3a8ab8a7ab049e66f29d6f8c85a244226
Carter-Wu <carter.wu@stonybrook.edu>
2024-11-18 01:04:51 +0000

    E

--- src/hw4.c
+++ src/hw4.c
@@ -535,7 +535,7 @@ int main()
                 }
                 if (parameter_count < 20) {
                     memset(buffer, 0, BUFFER_SIZE);
-                    strcpy(buffer, "E tt 201"); //invalid parameters
+                    strcpy(buffer, "E %d 201", &parameter_count); //invalid parameters
                     send(conn_fd, buffer, strlen(buffer), 0);
                 }
                 if (parameter_count == 20) {
